40db8e9723554e548c2f8cdea0851ba7d5f910e4
Michael Parker <michaelparker@NYFCs-MacBook-Air.local>
2019-06-26 20:53:44 -0400

    Completed all tasks and stretch tasks, except one

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -3,23 +3,38 @@
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
-// myFunction();
+const myFunction = () => {
+    console.log("Function was invoked!");
+}
+myFunction();
+
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
-// anotherFunction("Example");
+const anotherFunction = (param) => {
+    return param;
+}
+const result = anotherFunction("Example");
+console.log(result);
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+const add = (param1, param2) => {
+    return param1 + param2;
+}
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+const subtract = (param1, param2) => {
+    return param1-param2;
+}
+console.log(subtract(1,2));
 
 // Stretch
 
@@ -27,4 +42,11 @@
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
+// console.log(triple);
+//
+// my failed attempt:
+// exampleArray = [1,2,3,4];
+// const triple = (num) => {
+//     return exampleArray.map(num*3);
+// }
 // console.log(triple);
